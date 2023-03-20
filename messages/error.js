a4a2d8a3d150be2a2efd7b2d2fc3209f2e3666a7
fenominal <rakupatel219@gmail.com>
2023-03-20 12:55:42 +0530

    Update

--- messages/error.js
+++ messages/error.js
@@ -1,3 +1,57 @@
-export const INVALID_PLATFORM ="Please Enter Platfrom name like :- FaceBook/ Instagram/ Twitter";
+export const INVALID_PLATFORM =
+  "Please Enter Platfrom name like :- FaceBook/ Instagram/ Twitter";
 
-export const INVADLI_TOKEN= "Unauthorized User....";
+export const INVADLI_TOKEN = "Unauthorized User....";
+
+// Success Messages...
+export const SIGNIN_SUCCESS = "Sign In Succesfull...";
+export const LOGIN_SUCCESS = "Login Succesful...";
+
+//Invalid Error Messages..
+export const INVALID_TOKEN_ERROR =
+  "You Are Not Authorized To Perform This Action...";
+export const INVADLI_EMAIL = "Invalid Email...";
+export const INVALID_PASSWORD =
+  "Password Should be In One Upercase , One Lowercase, One Number and Length is between 8 to 15...";
+export const INVALID_PHONE = "Invalid Phone Number...";
+export const NOT_ANUMBER_PHONE = "Invalid Phone Number....";
+export const NOT_ANUMBER_PROQTY = "Invalid Product Qunetity...";
+export const USER_ID = "Requested User Id Is Not Valid Please Check IT...";
+export const PRODCUT_ID =
+  "Requested Prodcut Id Is Not Valid Please Check IT...";
+export const ORDER_ID = "Requested Order Id Is Not Valid Please Check IT...";
+
+// Empty Filed Error Message...
+export const EMPTY_SIGNUP_ALL =
+  "Please Enter Value In email, password, conform_password, mobile ...";
+export const EMPTY_LOGIN_ALL = "Please Enter Email And Password...";
+export const EMPTY_EMAIL = "Please Enter Email ...";
+export const EMPTY_PASSWORD = "Please Enter Password ...";
+export const EMPTY_FULLNAME = "Please Enter Full Name...";
+export const EMPTY_CONFORM_PASSWORD = "Please Enter Conform Password ...";
+export const EMPTY_CONFORM_MOBILE = "Please Enter Mobile Number ...";
+export const EMPTY_TOKEN = "Token Requierd ...";
+export const EMPTY_USERID = "Please Enter User ID...";
+export const EMPTY_DATA = "NO Data Found...";
+export const EMPTY_PRODCUTID = "Please Enter Prodcut Id.....";
+export const EMPTY_ORDERID = "Please Enter Order Id.....";
+
+// Alredy Used Messages.
+export const USER_EXISTE = "Email is alredy exist...";
+export const USER_NOTEXISTE = "User Not Exist in System...";
+export const PRODCUT_NOTEXISTE = "Prodcut Not Existe...";
+export const EMAIL_USED = "Entered the same email used as earlier...";
+export const NUMBER_EXISTE = "Mobile Number Alredy Existe...";
+export const PASS_CPASS_MISSMATCH =
+  "Password And Confrom Password Not Matched...";
+
+//Catch Error
+export const SWW = "Something Went Worng...";
+
+//Incorrect Messages.
+export const INCORRECT_EMAIL = "Email Not Exist...";
+export const INCORRECT_PASS = "Incorect Password...";
+
+//
+export const INSERT_PLATFORM =
+  "Please Enter Platfrom name like :- FaceBook/ Instagram/ Twitter";
